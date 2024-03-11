379e80868596ebae9d13c8a6df9b82ea98386faf
Joshua <celisjoshleon@gmail.com>
2024-03-10 23:56:35 -0500

    feat: :sparkles: Migration to DB
    Entities migration to DB

--- API/Program.cs
+++ API/Program.cs
@@ -71,5 +71,5 @@ app.MapControllers();
 
 app.Run();
 
-//dotnet ef database update --project ./Persistence/ --startup-project ./API/
-//dotnet ef migrations add InitialCreate --project .\Persistence\ --startup-project ./API/ --output-dir ./Data/Migrations
+//dotnet ef database update --project ./DataAccess/ --startup-project ./API/
+//dotnet ef migrations add InitialCreate --project .\DataAccess\ --startup-project ./API/ --output-dir ./Data/Migrations
